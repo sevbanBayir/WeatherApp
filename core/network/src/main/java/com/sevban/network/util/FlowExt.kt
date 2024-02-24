efceab61be447b3810054a9fce3bf00c364b4513
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-24 22:05:09 +0300

    added todo

--- core/network/src/main/java/com/sevban/network/util/FlowExt.kt
+++ core/network/src/main/java/com/sevban/network/util/FlowExt.kt
@@ -12,10 +12,10 @@ import retrofit2.Response
 fun <T, Model> Flow<Response<T>>.asRestApiCall(mapper: (T) -> Model): Flow<Model> =
     map { response ->
         response.run {
-
             if (isSuccessful) {
                 body() ?: throw Failure(ErrorType.EMPTY_RESPONSE, errorResponse = null)
             } else {
+                //TODO: EXPENSIVE OPERATION !!!
                 val gson = GsonBuilder().create()
                 val errorResponse = gson.fromJson(errorBody()?.string(), ErrorResponse::class.java)
 
