ed8c55c458ed2817f4d4df01589348f3455db195
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-25 18:51:08 +0300

    remvod dummy character and its mapper

--- core/data/src/main/java/com/sevban/data/mapper/CharacterMapper.kt
+++
@@ -1,9 +0,0 @@
-package com.sevban.data.mapper
-
-import com.sevban.model.Character
-import com.sevban.network.model.CharacterDTO
-
-fun CharacterDTO.toCharacter() = Character(
-    id = id.toString(),
-    name = name.orEmpty(),
-)
