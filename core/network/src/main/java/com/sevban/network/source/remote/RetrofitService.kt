d3445229f4d54531ef37e2e7a5cf403d04320f64
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-25 18:47:19 +0300

    created new api data source interface & implementation

--- core/network/src/main/java/com/sevban/network/source/remote/RetrofitService.kt
+++ core/network/src/main/java/com/sevban/network/source/remote/RetrofitService.kt
@@ -1,15 +1,30 @@
 package com.sevban.network.source.remote
 
 import com.sevban.network.model.CharacterDTO
-import com.sevban.network.model.CharactersDTO
+import com.sevban.network.model.forecast.ForecastDTO
+import com.sevban.network.model.weather.WeatherDTO
+import com.sevban.network.util.Constants.LANGUAGE
+import com.sevban.network.util.Constants.METRIC
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RetrofitService {
 
-    @GET("character")
-    suspend fun getAllCharacters(): CharactersDTO
+    @GET("data/2.5/weather")
+    suspend fun getLocationWeather(
+        @Query("units") units: String = METRIC,
+        @Query("lat") lat: String,
+        @Query("long") long: String,
+        @Query("lang") lang: String = LANGUAGE
+    ): Response<WeatherDTO>
 
-    @GET("character/{id}")
-    suspend fun getCharacterById(@Path("id") id: Int): CharacterDTO
+    @GET("data/2.5/forecast")
+    suspend fun getLocationForecast(
+        @Query("units") units: String = METRIC,
+        @Query("lat") lat: String,
+        @Query("long") long: String,
+        @Query("lang") lang: String = LANGUAGE
+    ): Response<ForecastDTO>
 }
