1238e87ddd06c302dd82e50f4010787d4e578989
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-25 19:02:30 +0300

    removed dummy usecase

--- feature/detail/src/main/java/com/sevban/detail/DetailViewModel.kt
+++ feature/detail/src/main/java/com/sevban/detail/DetailViewModel.kt
@@ -1,7 +1,6 @@
 package com.sevban.detail
 
 import androidx.lifecycle.ViewModel
-import com.sevban.domain.usecase.GetCharacterUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -9,7 +8,6 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(
-    private val getCharacterUseCase: GetCharacterUseCase,
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(UiState())
