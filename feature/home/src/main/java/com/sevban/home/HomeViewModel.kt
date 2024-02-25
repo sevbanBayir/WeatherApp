213d3f78c750775e302eb84127b32809800171c6
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-25 19:03:01 +0300

    new ui implementations

--- feature/home/src/main/java/com/sevban/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/sevban/home/HomeViewModel.kt
@@ -2,11 +2,9 @@ package com.sevban.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.sevban.domain.usecase.GetCharacterListUseCase
-import com.sevban.domain.usecase.GetCharacterUseCase
-import com.sevban.model.Character
+import com.sevban.domain.usecase.GetWeatherUseCase
+import com.sevban.model.Weather
 import com.sevban.network.Failure
-import com.sevban.network.util.ErrorResponse
 import com.sevban.ui.util.handleFailures
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
@@ -14,18 +12,13 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val getCharacterListUseCase: GetCharacterListUseCase,
-    private val getCharacterUseCase: GetCharacterUseCase
+    private val getWeatherUseCase: GetWeatherUseCase,
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(UiState())
@@ -34,7 +27,7 @@ class HomeViewModel @Inject constructor(
     private val _error = Channel<Failure>()
     val error = _error.receiveAsFlow()
 
-    val characterState: StateFlow<Character?> = getCharacterUseCase.execute("1")
+    val characterState: StateFlow<Weather?> = getWeatherUseCase.execute("1", "2")
         .handleFailures {
             _error.send(it)
         }
--- feature/home/src/main/java/com/sevban/home/UiState.kt
+++ feature/home/src/main/java/com/sevban/home/UiState.kt
@@ -1,7 +1,5 @@
 package com.sevban.home
 
-import com.sevban.model.Character
-
 data class UiState (
     val characterList: List<Character> = emptyList(),
 )
