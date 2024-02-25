29975841337ebe36fa658562a129605ea5d8cb70
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-25 18:48:35 +0300

    removed dummy repository interface & implementation

--- core/data/src/main/java/com/sevban/data/repository/Repository.kt
+++
@@ -1,9 +0,0 @@
-package com.sevban.data.repository
-
-import com.sevban.model.Character
-import kotlinx.coroutines.flow.Flow
-
-interface Repository {
-    fun getCharacterList() : Flow<List<Character>>
-    fun getCharacterById(id: String) : Flow<Character>
-}
