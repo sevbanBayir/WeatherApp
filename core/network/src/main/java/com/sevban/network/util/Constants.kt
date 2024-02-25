ef72379861ee1079d673ab97d4eb692b3fb5f7c7
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-25 18:44:55 +0300

    created Constants.kt for api constants

---
+++ core/network/src/main/java/com/sevban/network/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.sevban.network.util
+
+object Constants {
+
+    const val METRIC = "metric"
+    const val LANGUAGE = "en"
+
+    const val BASE_URL = "https://api.openweathermap.org/"
+}
