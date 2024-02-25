7e23789895a822eb8bce5b439abdb16f6dd3194c
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-25 19:01:49 +0300

    cerated WeatherMapper.kt

--- core/data/src/main/java/com/sevban/data/mapper/WeatherMapper.kt
+++ core/data/src/main/java/com/sevban/data/mapper/WeatherMapper.kt
@@ -1,8 +1,6 @@
 package com.sevban.data.mapper
 
-import com.sevban.model.Character
 import com.sevban.model.Weather
-import com.sevban.network.model.CharacterDTO
 import com.sevban.network.model.weather.WeatherDTO
 
 fun WeatherDTO.toWeather() = Weather(
