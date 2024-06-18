7de52617117b987f6157242f7c8b56d867c19073
Sevban Bayır <sevbanbuyer@gmail.com>
2024-06-18 17:36:57 +0300

    Revert "feature: make adjustments to line chart"
    This reverts commit 74d1bed93fe50bf35d754f0c47481ff7d4d114d2.

--- feature/home/src/main/java/com/sevban/home/components/forecastquadrant/ForecastQuadrant.kt
+++ feature/home/src/main/java/com/sevban/home/components/forecastquadrant/ForecastQuadrant.kt
@@ -1,7 +1,7 @@
 package com.sevban.home.components.forecastquadrant
 
-import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,8 +10,6 @@ import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.text.drawText
-import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlin.random.Random
@@ -21,46 +19,41 @@ fun ForecastQuadrant(
     temperatures: List<Int>,
     modifier: Modifier = Modifier
 ) {
-    val textMeasurer = rememberTextMeasurer()
 
-    Canvas(
-        modifier = modifier.size(400.dp)
-    ) {
+    Canvas(modifier = Modifier
+        .size(400.dp)
+        .border(1.dp, Color.Red)) {
         val graphDepth = size.height
-        Log.d("Graph Depth", graphDepth.toString())
-        val oneDegree = graphDepth / (temperatures.max() - temperatures.min())
-        Log.d("One Degree", oneDegree.toString())
-        Log.d(
-            "Temperatures",
-            temperatures.toString() + "Max: ${temperatures.max()} Min: ${temperatures.min()}"
-        )
+        val oneDegree = graphDepth / 6.dp.toPx()
+        val oneInterval = size.width / 8f
 
-        val oneInterval = size.width / temperatures.size
-
-        var xCursor = 0f
-        var yCursor = graphDepth - ((temperatures.first() - temperatures.min()) * oneDegree)
+        var xCursor = oneInterval
+        var yCursor = graphDepth - (temperatures.first() * oneDegree)
 
         val lineStroke = 6f
         val jointRadius = 10f
+        val jointStroke = 6f
 
         val temperaturePath = Path().apply {
             moveTo(0f, yCursor)
-            temperatures.forEach { value ->
-                val textResult = textMeasurer.measure(value.toString())
-                yCursor = graphDepth - ((value - temperatures.min()) * oneDegree)
-                drawText(textResult, topLeft = Offset( -20f-textResult.firstBaseline, yCursor))
+            temperatures.drop(1).forEach {
+                yCursor = graphDepth - (it * oneDegree)
                 lineTo(xCursor, yCursor)
                 drawLine(Color.Gray, start = Offset(xCursor, 0f), end = Offset(xCursor, graphDepth))
                 drawCircle(
                     color = Color.Black,
                     radius = jointRadius,
                     center = Offset(xCursor, yCursor),
+                    style = Stroke(jointStroke),
                 )
                 moveTo(xCursor, yCursor)
                 xCursor += oneInterval
             }
         }
 
+        repeat(temperatures.size) {
+
+        }
         drawPath(
             temperaturePath,
             color = Color.Gray,
@@ -75,21 +68,60 @@ fun ForecastQuadrant(
 @Preview(showBackground = true)
 @Composable
 private fun ForecastQuadrantPrev() {
-    ForecastQuadrant(temperatures = generateTemperatureList(20, 32, 8))
+    ForecastQuadrant(temperatures = createRandomDailyTemps())
 }
 
-fun generateTemperatureList(minTemp: Int, maxTemp: Int, maxNumber: Int): List<Int> {
-    require(minTemp <= maxTemp) { "Minimum temperature must be less than or equal to maximum temperature" }
-    require(maxNumber > 0) { "Number of elements must be greater than zero" }
-
-    return List(maxNumber) { Random.nextInt(minTemp, maxTemp + 1) }
+fun createRandomDailyTemps(): List<Int> {
+    val mutableList = mutableListOf<Int>()
+    for (i in 1..9) {
+        if (Random.nextBoolean())
+            mutableList.add(i + 1)
+        else
+            mutableList.add(i - 2)
+    }
+    println(mutableList)
+    return mutableList
 }
 
-fun main() {
-    val minTemp = 20  // Minimum temperature in Celsius
-    val maxTemp = 35   // Maximum temperature in Celsius
-    val maxNumber = 8 // Number of temperature readings to generate
-
-    val temperatureList = generateTemperatureList(minTemp, maxTemp, maxNumber)
-    println("Generated temperature list: $temperatureList")
-}
+val randomList = listOf(
+    10,
+    20,
+    30,
+    40,
+    50,
+    60,
+    70,
+    80,
+    /*    15,
+        10,
+        19,
+        17,
+        11,
+        25,
+        -7,
+        3,
+        49,
+        54,
+        10,
+        11,
+        -8,
+        40,
+        21,
+        43,
+        -11,
+        59,
+        -5,
+        -10,
+        33,
+        57,
+        53,
+        34,
+        -1,
+        -5,
+        18,
+        54,
+        -14,
+        7,
+        -5,
+        26*/
+)
