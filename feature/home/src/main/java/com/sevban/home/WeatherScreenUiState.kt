f546060c70540a66dac5285360d42c0099a09238
Sevban Bayır <sevbanbuyer@gmail.com>
2024-09-22 12:41:04 +0300

    refactor: new state management logic added

--- feature/home/src/main/java/com/sevban/home/WeatherScreenUiState.kt
+++ feature/home/src/main/java/com/sevban/home/WeatherScreenUiState.kt
@@ -1,20 +1,5 @@
 package com.sevban.home
 
-import com.sevban.model.Forecast
-import com.sevban.model.Weather
-
-data class WeatherScreenUiState (
-    val weather: WeatherUiModel? = null,
-    val forecast: Forecast? = null,
-    val isLoading: Boolean = false,
+data class WeatherScreenUiState(
     val shouldShowPermanentlyDeclinedDialog: Boolean = false
 )
-
-/*sealed interface WeatherScreenUiState {
-    data object Loading: WeatherScreenUiState
-    data class Success(
-        val weather: Weather? = null,
-        val forecast: Forecast? = null,
-    )
-}
-*/
