3038dc462ab1683aba26d232564844907c3e73d4
Sevban Bayir <sevban.bayir@d-teknoloji.com.tr>
2025-02-04 22:03:13 +0300

    chore: improve and simplify error handling logic

--- feature/home/src/main/java/com/sevban/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/sevban/home/HomeViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sevban.common.location.LocationObserver
 import com.sevban.common.location.MissingLocationPermissionException
+import com.sevban.common.model.ErrorType
+import com.sevban.common.model.Failure
 import com.sevban.domain.usecase.GetForecastUseCase
 import com.sevban.domain.usecase.GetWeatherUseCase
 import com.sevban.home.mapper.toForecastUiModel
@@ -12,18 +14,26 @@ import com.sevban.home.model.WeatherScreenUiState
 import com.sevban.home.model.WeatherState
 import com.sevban.home.model.toWeatherUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ChannelIterator
+import kotlinx.coroutines.channels.ChannelResult
+import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.retry
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.selects.SelectClause1
+import kotlinx.coroutines.selects.SelectClause2
 import javax.inject.Inject
 import kotlin.time.Duration.Companion.seconds
 
@@ -39,7 +49,7 @@ class HomeViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(WeatherScreenUiState())
     val uiState = _uiState.asStateFlow()
 
-    private val _error = Channel<Throwable>()
+    private val _error = Channel<Failure>()
     val error = _error.receiveAsFlow()
 
     val weatherState = combine(
@@ -54,13 +64,7 @@ class HomeViewModel @Inject constructor(
         }
     }
         .retry { cause ->
-            if (cause is MissingLocationPermissionException) {
-                delay(3.seconds)
-                true
-            } else {
-                _error.send(cause)
-                false
-            }
+            (cause is MissingLocationPermissionException).also { if (it) delay(3.seconds) }
         }
         .flatMapLatest { (latitude, longitude) ->
             combine(
@@ -78,7 +82,8 @@ class HomeViewModel @Inject constructor(
                     forecast = forecast.toForecastUiModel()
                 )
             }
-        }.stateIn(
+        }
+        .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5000),
             initialValue = WeatherState.Loading
