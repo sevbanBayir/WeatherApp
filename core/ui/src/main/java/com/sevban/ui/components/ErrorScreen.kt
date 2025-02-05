1b70d9d12e739e766b9ff9b16dbb1085ff66b4f3
Sevban Bayir <sevban.bayir@d-teknoloji.com.tr>
2025-02-05 12:00:17 +0300

    feat: separate ErrorScreen.kt

---
+++ core/ui/src/main/java/com/sevban/ui/components/ErrorScreen.kt
@@ -0,0 +1,52 @@
+package com.sevban.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.sevban.common.model.Failure
+import com.sevban.common.model.toLocalizedMessage
+import com.sevban.ui.R
+
+@Composable
+fun ErrorScreen(
+    whenErrorOccurred: suspend (Throwable, String?) -> Unit,
+    failure: Failure,
+    onTryAgainClick: () -> Unit
+) {
+    val context = LocalContext.current
+
+    LaunchedEffect(key1 = true) {
+        whenErrorOccurred(
+            failure,
+            failure.errorType.toLocalizedMessage(context)
+        )
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(16.dp).fillMaxSize()
+    ) {
+        Text(
+            text = failure.errorType.toLocalizedMessage(context),
+            style = MaterialTheme.typography.titleLarge
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextButton(onClick = onTryAgainClick) {
+            Text(text = stringResource(R.string.try_again))
+        }
+    }
+}
--- feature/home/src/main/java/com/sevban/home/HomeScreen.kt
+++ feature/home/src/main/java/com/sevban/home/HomeScreen.kt
@@ -1,33 +1,21 @@
 package com.sevban.home
 
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.sevban.common.extensions.openAppSettings
-import com.sevban.common.model.Failure
-import com.sevban.common.model.toLocalizedMessage
 import com.sevban.home.components.WeatherContent
 import com.sevban.home.model.WeatherScreenUiState
 import com.sevban.home.model.WeatherState
+import com.sevban.ui.components.ErrorScreen
 import com.sevban.ui.components.LoadingScreen
 import com.sevban.ui.components.PermissionAlertDialog
 import com.sevban.ui.components.PermissionRequester
@@ -106,39 +94,6 @@ fun HomeScreen(
                     }
                 )
             }
-        }
-    }
-}
-
-@Composable
-fun ErrorScreen(
-    whenErrorOccurred: suspend (Throwable, String?) -> Unit,
-    failure: Failure,
-    onTryAgainClick: () -> Unit
-) {
-    val context = LocalContext.current
-
-    LaunchedEffect(key1 = true) {
-        whenErrorOccurred(
-            failure,
-            failure.errorType.toLocalizedMessage(context)
-        )
-    }
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxSize()
-    ) {
-        Text(
-            text = failure.errorType.toLocalizedMessage(context),
-            style = MaterialTheme.typography.titleLarge
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        TextButton(onClick = onTryAgainClick) {
-            Text(text = stringResource(R.string.try_again))
         }
     }
 }
