da47456d4c6ec699e12763ce83f8c586158aad40
Sevban Bayir <sevban.bayir@d-teknoloji.com.tr>
2025-02-05 23:16:49 +0300

    feat: added nice icons

--- feature/home/src/main/java/com/sevban/home/components/LocationAndFetTimeBox.kt
+++ feature/home/src/main/java/com/sevban/home/components/LocationAndFetTimeBox.kt
@@ -3,6 +3,7 @@ package com.sevban.home.components
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Icon
@@ -14,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.sevban.designsystem.theme.WeatherAppIcons
 import com.sevban.home.R
 import com.sevban.home.model.WeatherUiModel
 
@@ -34,7 +36,8 @@ fun LocationAndFetTimeBox(
             contentPadding = PaddingValues(0.dp),
         ) {
             Icon(
-                imageVector = Icons.Default.LocationOn,
+                imageVector = WeatherAppIcons.Location,
+                modifier = Modifier.size(24.dp),
                 contentDescription = stringResource(id = R.string.cd_location_icon)
             )
             Text(
