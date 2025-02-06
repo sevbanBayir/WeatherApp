aa9dacc423477c3345997fc86488ac78ea0025fd
Sevban Bayir <sevban.bayir@d-teknoloji.com.tr>
2025-02-06 12:19:15 +0300

    chore: update model module

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -2,15 +2,9 @@ plugins {
     id(libs.plugins.java.library.get().pluginId)
     alias(libs.plugins.org.jetbrains.kotlin.jvm)
 }
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = "18"
-    }
-}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_18
     targetCompatibility = JavaVersion.VERSION_18
-}
-java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(18))
 }
